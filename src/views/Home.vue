c88855f24074f4b3f1c8d885e02194c76a14f005
Gints Polis <gints.polis@lattelecom.lv>
2019-04-21 22:27:25 +0300

    Removed linting errors.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,14 +8,14 @@
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script>
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue { }
 </script>
