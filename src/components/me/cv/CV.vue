2596bbee9f8837699214521d563f48dbcd271304
Gints Polis <gints.polis@lattelecom.lv>
2019-04-28 17:28:56 +0300

    CV one variant

--- src/components/me/cv/CV.vue
+++ src/components/me/cv/CV.vue
@@ -1,5 +1,88 @@
 <template>
   <v-container>
-    <h1>This is cv page</h1>
+    <v-card>
+      <v-toolbar
+        color="primary"
+        dark
+      >
+        <v-toolbar-title>Profile</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <emp-employee-details></emp-employee-details>
+      </v-card-text>
+    </v-card>
+    <br>
+    <v-card>
+      <v-toolbar
+        color="primary"
+        dark
+      >
+        <v-toolbar-title>Experience</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          dark
+          icon
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <emp-experience></emp-experience>
+      </v-card-text>
+    </v-card>
+    <br>
+    <v-card>
+      <v-toolbar
+        color="primary"
+        dark
+      >
+        <v-toolbar-title>Education</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          dark
+          icon
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <emp-education></emp-education>
+      </v-card-text>
+    </v-card>
+    <br>
+    <v-card>
+      <v-toolbar
+        color="primary"
+        dark
+      >
+        <v-toolbar-title>Skills</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn
+          dark
+          icon
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-card>
   </v-container>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Inject } from 'vue-property-decorator'
+import EmpEmployeeDetails from './EmpEmployeeDetails.vue'
+import EmpEducation from './EmpEducation.vue'
+import EmpExperience from './EmpExperience.vue'
+
+@Component({
+  name: 'CV',
+  components: {
+    EmpEmployeeDetails,
+    EmpEducation,
+    EmpExperience
+  }
+})
+export default class CV extends Vue { }
+
+</script>
